371e1962d2c4563aa6fda262525549843ad38af2
JosephLindzius <joseph.lindzius@gmail.com>
2019-09-19 14:58:39 +0200

    geo fix, img

--- assets/js/geolocator.js
+++ assets/js/geolocator.js
@@ -1,27 +1,30 @@
 var positionArray = [];
+
 function getCoords () {
-    navigator.geolocation.getCurrentPosition(function(position){
+    navigator.geolocation.getCurrentPosition(function (position) {
         positionArray.push(position.coords.latitude);
         positionArray.push(position.coords.longitude);
+        axios.get("https://api.opencagedata.com/geocode/v1/json?key=6301d53481104202bdda74b2d76ae44f&q="+positionArray[0]+"%2C+"+positionArray[1]+"&pretty=1&no_annotations=1")
+            .then(function(response){
+                let city = response.data.results[0].components.city;
+                console.log(city);
 
+                var result = confirm("Add current location to box? "+ city);
+                if (result == true) {
+                    document.getElementById("input").value = city;
+                } else {
+                    alert("Cancel was pressed.");
+                }
+            })
+            .catch(function(error){
+                alert("error")
+            })
     });
 }
 
-getCoords();
-
-//positionArray = [51.509865, -0.118092];
-function showCity (){
-    axios.get("https://api.opencagedata.com/geocode/v1/json?key=6301d53481104202bdda74b2d76ae44f&q="+positionArray[0]+"%2C+"+positionArray[1]+"&pretty=1&no_annotations=1")
-        .then(function(response){
-            console.log(response);
-            document.getElementById("input").value = response.data.results[0].components.city;
-        })
-        .catch(function(error){
-            alert("error")
-        })
-}
-
-//showCity();
+document.getElementById("geo").addEventListener("click", function(){
+    getCoords();
+});
 
 
 
